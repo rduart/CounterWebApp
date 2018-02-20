f902298779b7addc1022d57faf52f88192bf6963
rduart <rduart@users.noreply.github.com>
2018-02-20 15:18:42 -0800

    Updated Feature B

--- CounterWebApp/src/main/java/com/mkyong/controller/BaseController.java
+++ CounterWebApp/src/main/java/com/mkyong/controller/BaseController.java
@@ -1,11 +1,8 @@
-
 package com.mkyong.controller;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.ui.ModelMap;
-
 import org.springframework.web.bind.annotation.PathVariable;
 
 import org.springframework.web.bind.annotation.RequestMapping;
